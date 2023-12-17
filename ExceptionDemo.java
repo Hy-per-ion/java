7349c90f4bed7882b736b9d3a0d1936e7fdbd2e9
Kaushik <122469210+Hy-per-ion@users.noreply.github.com>
2023-12-18 01:05:17 +0530

    Add files via upload

---
+++ ExceptionDemo.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class ExceptionDemo {
+    static void func(int a, int b) throws ArithmeticException, ArrayIndexOutOfBoundsException {
+        System.out.println("Division result: " + (a / b));
+        int[] arr = {1, 2, 3};
+        System.out.println("Array element at index " + b + ": " + arr[b]);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            try {
+                System.out.print("Enter the value for 'a': ");
+                int a = in.nextInt();
+
+                System.out.print("Enter the value for 'b': ");
+                int b = in.nextInt();
+
+                func(a, b);
+            } catch (ArithmeticException e) {
+                System.out.println("Can't divide by zero");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Out of bounds!");
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter an integer.");
+                in.nextLine(); // Clear the input buffer
+            }
+        }
+    }
+}
